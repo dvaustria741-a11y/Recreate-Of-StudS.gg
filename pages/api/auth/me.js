e47ec21021908609748cc4d123adeab891972e46
dvaustria741-a11y <dvaustria741@gmail.com>
2026-05-19 10:06:52 +0800

    Add API endpoint to get user session details

---
+++ pages/api/auth/me.js
@@ -0,0 +1,13 @@
+import { getSession } from '../../lib/session'
+
+export default function handler(req, res) {
+  const session = getSession(req)
+  if (!session) return res.status(401).json({ user: null })
+  res.json({
+    user: {
+      userId: session.userId,
+      username: session.username,
+      avatar: session.avatar,
+    }
+  })
+}
